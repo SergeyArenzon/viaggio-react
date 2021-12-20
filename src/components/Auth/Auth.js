5f61701b42ddc7f11945dd6b1a281cd821bec216
Sergey Arenzon <sergeyr1991@gmail.com>
2021-12-20 22:03:52 +0200

    Auto stash before rebase of "refs/heads/app-rewrite"

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,102 +1,126 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { userSchema } from '../../validations/user';
-
-
-
+import React, { useState, useRef, useEffect } from "react";
+import { userSchema } from "../../validations/user";
 
 export default function Auth() {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const lastNameRef = useRef();
-    const firstNameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const lastNameRef = useRef();
+  const firstNameRef = useRef();
 
-    const [loading, setLoading] = useState(true);
-    const [signUpMode, setSignUpMode] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [signUpMode, setSignUpMode] = useState(true);
 
+  /////////////////////
+  //  Signout method //
+  /////////////////////
+  const registerHandler = async (event) => {
+    event.preventDefault();
 
-    /////////////////////
-    //  Signout method //
-    /////////////////////
-    const registerHandler = async (event) => {
-        event.preventDefault();
+    const data = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+    };
+    const isValid = await userSchema.isValid(data);
 
-        const data = {
-            email: emailRef.current.value,
-            password: passwordRef.current.value,
-            firstName: firstNameRef.current.value,
-            lastName: lastNameRef.current.value,
-        };
-        const isValid = await userSchema.isValid(data);
+    if (!isValid) {
+      alert("Creds isnt valid!");
+      return;
+    }
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/register`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        if (!isValid) {
-            alert('Creds isnt valid!');
-            return;
-        }
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/location/register`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+    if (response.status === 201) {
+      // router.replace('/');
+      const res = await response.json();
+      return res;
+    }
+  };
 
-        if(response.status === 201){
-            // router.replace('/');
-            const res = await response.json();
-            return res;
-        }
+  ////////////////////
+  //  Signin method //
+  ////////////////////
+  const loginHandler = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
     };
 
-    ////////////////////
-    //  Signin method //
-    ////////////////////
-    const loginHandler = async (event) => {
-        event.preventDefault();
-
-        const data = {
-            email: emailRef.current.value,
-            password: passwordRef.current.value,
-        };
-
-
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/location/login`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-
-    };
-
-    const signUpForm = (
-        <form onSubmit={registerHandler}>
-            <input type="email" ref={emailRef}></input>
-            <input type="password" ref={passwordRef}></input>
-            <input type="text" ref={firstNameRef}></input>
-            <input type="text" ref={lastNameRef}></input>
-            <button>register</button>
-        </form>
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/login`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
+  };
 
-    const logInForm = (
-        <form onSubmit={loginHandler}>
-            <input type="email" ref={emailRef}></input>
-            <input type="password" ref={passwordRef}></input>
-            <button>login</button>
-        </form>
-    );
+  const signUpForm = (
+    <form onSubmit={registerHandler}>
+      <input type="email" ref={emailRef}></input>
+      <input type="password" ref={passwordRef}></input>
+      <input type="text" ref={firstNameRef}></input>
+      <input type="text" ref={lastNameRef}></input>
+      <button>register</button>
+    </form>
+  );
 
-    return (
-        <div>
-            <button onClick={() => {
-       fetch(`${process.env.REACT_APP_API_URL}/user`, {credentials: "include"});
+  const logInForm = (
+    <form onSubmit={loginHandler}>
+      <input type="email" ref={emailRef}></input>
+      <input type="password" ref={passwordRef}></input>
+      <button>login</button>
+    </form>
+  );
 
-      }}>checkckkk</button>
-            {signUpMode ? signUpForm : logInForm}
-            <button onClick={() => setSignUpMode(!signUpMode)}>Switch</button>
-        </div>
-    );
+  const x = async () => {
+    const z = await fetch(`${process.env.REACT_APP_API_URL}/location/test`);
+    const f = await z.json();
+    console.log(f);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          fetch(`${process.env.REACT_APP_API_URL}/user`, {
+            credentials: "include",
+          });
+        }}
+      >
+        checkckkk
+      </button>
+
+      <button
+        onClick={async() => {
+            const s = `${process.env.REACT_APP_API_URL}/logout`;
+            console.log(s);
+           fetch(s, {
+            credentials: "include",
+          }).then((res) => console.log(res));
+        }}
+      >
+        logout
+      </button>
+      <button onClick={x}>test</button>
+
+      {signUpMode ? signUpForm : logInForm}
+      <button onClick={() => setSignUpMode(!signUpMode)}>Switch</button>
+    </div>
+  );
 }
