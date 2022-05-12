492eb2bd0b5e6c73f9ce9d6aa766b4ce9fd4995b
Sergey Arenzon <sergeyr1991@gmail.com>
2022-05-12 16:57:37 +0300

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Home from "./components/Home/Home";
 import Layout from "./containers/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
