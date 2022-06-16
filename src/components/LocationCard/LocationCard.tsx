a2a5415ed1c324720ab02cab1bea799ce1a250c7
Sergey Arenzon <sergeyr1991@gmail.com>
2022-06-16 14:00:48 +0300

    fix project fonts var

--- src/components/LocationCard/LocationCard.tsx
+++ src/components/LocationCard/LocationCard.tsx
@@ -1,6 +1,6 @@
 import './LocationCard.scss';
-import noBackground from "../../assets/images/no-background.jpg"
-import {Link} from 'react-router-dom'
+import noBackground from "../../assets/images/no-background.jpg";
+import {Link} from 'react-router-dom';
 
 export default function LocationCard({ location }: {location: ILocation}) {
   let image = location.images.length > 0 ? `${ process.env.REACT_APP_API_URL}/location/${location.images[0]}/download` : noBackground
@@ -9,12 +9,12 @@ export default function LocationCard({ location }: {location: ILocation}) {
     <div className="location">
            <img className={`location__image ${location.images.length === 0 ? "location__image--empty" : ""}`} src={image} alt="location" />
               <div className="location__info">
-                <div className="location__name">
+                <div className="location__name location__title">
                   {location.name}
                 </div>
-                <div className="location__location">Where: {location.location}</div>
-                <p className="location__description">Description: {location.description}</p>
-                <div className="location__price">${location.price} Per night</div>
+                <div className="location__location"><span className="location__title">Where:</span> {location.location}</div>
+                <p className="location__description"><span className="location__title">Description:</span> {location.description}</p>
+                <div className="location__price">${location.price} <span className="location__title">Per night</span></div>
                 <div className="location__show">
 
                   <Link to={`/locations/${location.id}`}>SHOW</Link>
