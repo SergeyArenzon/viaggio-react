4aae2a517d6b9694e422d1febfb4ed406871dd25
Sergey Arenzon <sergeyr1991@gmail.com>
2022-09-24 17:13:38 +0300

    rating pg migrate

--- src/services/api/rating.ts
+++ src/services/api/rating.ts
@@ -1,9 +1,9 @@
 import { get, post, put, destroy } from "./config";
 
 const Rating = {
-    update: async (locationId: string, data: {rating: number}) => {
+    update: async (locationId: string, data:  number) => {
         try {
-          const response = await put(`${process.env.REACT_APP_API_URL}/location/${locationId}/rating`, data);
+          const response = await put(`${process.env.REACT_APP_API_URL}/location/${locationId}/rating`, {rating: data});
           return response
         } catch (error) {
           return  error ;
